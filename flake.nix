b29c054a1139b1745aa4abaaa65706f288e7d642
shiaic <16158572+shiaic@users.noreply.github.com>
2023-06-22 16:58:41 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+    neovim = {
+      url = "github:neovim/neovim?dir=contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    neovim
+  }: let
+    system = "x86_64-linux";
+    
+    # Overlay lets you override input pkgs or define new ones
+    # prev (attset argument) - original nixpkgs
+    # final - new package set
+    overlayFlakeInputs = prev: final: {
+      neovim = neovim.packages.${system}.neovim;
+    };
+    overlayNeovim = prev: final: {
+      myNeovim = import ./config.nix {
+        pkgs = final;
+      };
+    };
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ overlayFlakeInputs overlayNeovim ];
+  };
+
+  in {
+    packages.${system}.default = pkgs.neovim;
+    apps.${system}.default = {
+      type = "app";
+      program = "${pkgs.myNeovim}/bin/nvim";
+    };
+  };
+}
