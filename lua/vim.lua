16751fd6f00a6b6145a61a18943fce8e71f5fe64
shiaic <16158572+shiaic@users.noreply.github.com>
2023-07-05 08:25:34 -0700

    re-add vimrc stuff

--- lua/vim.lua
+++ lua/vim.lua
@@ -1,4 +1,46 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.o.hlsearch = false
+vim.wo.number = true
+vim.o.mouse = ''
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 2
+vim.o.smartindent = true
+vim.o.smarttab = true
+vim.o.expandtab = true
+vim.wo.signcolumn = 'yes'
 
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+vim.o.completeopt = 'menuone,noselect'
+
+vim.o.termguicolors = true
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
 
 require('kanagawa').setup({
   colors = {
@@ -17,4 +59,3 @@ vim.cmd("colorscheme kanagawa")
 require("lspconfig")
 require("nvim-tree")
 require("telescope")
---require("")
